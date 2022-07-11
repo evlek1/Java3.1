527c0f904336318e3f92c70e4050e9af7e0ddcd4
LEKtestQ@gmail.com <LEKtestQ@gmail.com>
2022-07-11 04:22:29 +0300

    Files Added

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int PriceCost = 1000;
+        int BonusMile = 20;
+        System.out.println("Количество бонусов равно " + (PriceCost / BonusMile));
+    }
+}
